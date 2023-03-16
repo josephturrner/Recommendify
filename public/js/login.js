562cdcd8f5205084eb3109eda6b1344f8869663f
sephturner <turnerj8@wit.edu>
2023-03-16 12:36:13 -0400

    New setup

---
+++ public/js/login.js
@@ -0,0 +1,126 @@
+var redirect_uri = 'http://localhost:8888/callback/';
+
+var client_id = 'de1edab117b649a58d1e84d1ef7ce560'; // Your client id
+var client_secret = '6232df2c5d28412b93837c891ac88214'; // Your secret
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
+
+const TOKEN = "https://accounts.spotify.com/api/token";
+const ARTISTS = "https://api.spotify.com/v1/me/top/artists?offset=0&limit=10&time_range=short_term"
+const TRACKS = "https://api.spotify.com/v1/me/top/tracks?offset=0&limit=10&time_range=short_term"
+
+const trackList = document.getElementById('track-list');
+
+function authorize() {
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-read-playback-state user-top-read";
+    window.location.href = url;
+}
+
+function onPageLoad() {
+    if (window.location.search.length > 0) {
+        handleRedirect();
+    }
+    else {
+        getSongs();
+    }
+}
+
+function handleRedirect() {
+    let code = getCode();
+    fetchAccessToken(code);
+    window.history.pushState("", "", redirect_uri);
+}
+
+function getCode() {
+    let code = null;
+    const query = window.location.search;
+    if (query.length > 0) {
+        const urlParams = new URLSearchParams(query);
+        code = urlParams.get('code')
+    }
+    return code;
+}
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthApi(body);
+}
+
+function callAuthApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthResponse;
+}
+
+function refreshAccessToken() {
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthApi(body);
+}
+
+function handleAuthResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        if (data.access_token != undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token != undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        getSongs();
+    } else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function getSongs() {
+    callAuthApi("GET", TRACKS, null, handleSongResponse);
+}
+
+function callApi (method, url, body, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("access_token"));
+    xhr.send(body);
+    xhr.onload = callback;
+}
+
+function handleSongResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        songList(data);
+    } else if (this.status == 401) {
+        refreshAccessToken();
+    } else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function songList(data) {
+    trackList.innerHTML = '';
+    for (i = 0; i < data.items.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = data.items[i].name;
+        trackList.appendChild(li);
+    }
+}
