aa67b0677d98c5f94215bf6947dba2ecbd8a45f0
Thomas Powell <powellt1@wit.edu>
2023-03-24 13:42:51 -0400

    Merge

--- app.js
+++ app.js
@@ -18,4 +18,40 @@ app.get('/callback', (req, res) => {
     res.render(__dirname + '/views/main.ejs');
 })
 
+app.post('/callback', async (req, res) => { 
+
+    const data = req.body; 
+
+    const result = await runPythonScript(data);
+
+    res.send(result); 
+
+});
+
+     
+
 app.listen(port, () => console.info(`http://localhost:${port}`));
+
+async function runPythonScript(data) { 
+
+    const { spawn } = require('child_process'); 
+
+    const pythonProcess = spawn('C:\\Users\\powellt1\\AppData\\Local\\Programs\\Python\\Python311\\python.exe', ['recommendation/model.py', JSON.stringify(data)]); /* Has to be python installation path */
+
+    return new Promise((resolve, reject) => { 
+
+        pythonProcess.stdout.on('data', (data) => { 
+
+            resolve(JSON.parse(data)); 
+
+        }); 
+
+        pythonProcess.stderr.on('data', (data) => { 
+
+            reject(new Error(`Error running Python script: ${data}`)); 
+
+        }); 
+
+    }); 
+
+} 
