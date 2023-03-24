3ced641dbc661ea24929fe3fc65e3e8e23535b01
sephturner <turnerj8@wit.edu>
2023-03-24 14:49:10 -0400

    done

--- app.js
+++ app.js
@@ -19,40 +19,40 @@ app.get('/callback', (req, res) => {
     res.render(__dirname + '/views/main.ejs');
 })
 
-app.post('/callback', async (req, res) => { 
+// app.post('/callback', async (req, res) => { 
 
-    const data = req.body; 
+//     const data = req.body; 
 
-    const result = await runPythonScript(data);
+//     const result = await runPythonScript(data);
 
-    res.send(result); 
+//     res.send(result); 
 
-});
+// });
 
      
 
 app.listen(port, () => console.info(`http://localhost:${port}`));
 
-async function runPythonScript(data) { 
+// async function runPythonScript(data) { 
 
-    const { spawn } = require('child_process'); 
+//     const { spawn } = require('child_process'); 
 
-    const pythonProcess = spawn('recommendation/python.exe', ['recommendation/model.py', JSON.stringify(data)]); /* Has to be python installation path */
+//     const pythonProcess = spawn('recommendation/python.exe', ['recommendation/model.py', JSON.stringify(data)]); /* Has to be python installation path */
 
-    return new Promise((resolve, reject) => { 
+//     return new Promise((resolve, reject) => { 
 
-        pythonProcess.stdout.on('data', (data) => { 
+//         pythonProcess.stdout.on('data', (data) => { 
 
-            resolve(JSON.parse(data)); 
+//             resolve(JSON.parse(data)); 
 
-        }); 
+//         }); 
 
-        pythonProcess.stderr.on('data', (data) => { 
+//         pythonProcess.stderr.on('data', (data) => { 
 
-            reject(new Error(`Error running Python script: ${data}`)); 
+//             reject(new Error(`Error running Python script: ${data}`)); 
 
-        }); 
+//         }); 
 
-    }); 
+//     }); 
 
-} 
+// } 
