3730872e362e99c1b78525c025f1097f5753f5b1
sephturner <turnerj8@wit.edu>
2023-04-06 12:01:04 -0400

    Finished documenting

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Use express
 const express = require("express");
 const app = express();
 const port = 8888;
 
+// Use the following for directories
 app.use(express.static('/public/'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
@@ -9,14 +11,17 @@ app.use('/images', express.static(__dirname + '/public/images'));
 app.use('/callback', express.static(__dirname + '/public'));
 app.use(express.json());
 
+// Landing page
 app.get('', (req, res) => {
   res.render(__dirname + '/views/landingpage.ejs');
 })
 
+// After login
 app.get('/callback', (req, res) => {
   res.render(__dirname + '/views/main.ejs');
 })
 
+// Listen for requests
 app.listen(port, () => console.info(`http://localhost:${port}`));
 
 
